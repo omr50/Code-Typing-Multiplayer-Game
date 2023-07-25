1276876daf4462b5dac342ec6e494b02d9cbb9f6
Omr50 <omrab4@gmail.com>
2023-07-25 01:01:46 -0400

    First commit

---
+++ user-service/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { startSender } from './mqSender';
+import { startReceiver } from './mqClient';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', async (req, res) => {
+  try {
+      await startReceiver();
+      await startSender();
+      res.send('Hello, user service!');
+  } catch (err: any ) {
+      res.status(500).send('An error occurred: ' + err.toString());
+  }
+});
+
+app.listen(port, () => {
+    console.log(`Usser service listening at http://localhost:${port}`);
+});
---
+++ user-service/src/mqClient.ts
@@ -0,0 +1,28 @@
+import * as amqp from 'amqplib/callback_api';
+
+export function startReceiver(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    amqp.connect('amqp://rabbitmq', (err: any, conn: amqp.Connection) => {
+      if (err) {
+        reject(err);
+      } else {
+        conn.createChannel((err: any, ch: amqp.Channel) => {
+          if (err) {
+            reject(err);
+          } else {
+            const q = 'my_queue';
+
+            ch.assertQueue(q, {durable: false});
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+            
+            ch.consume(q, (msg: amqp.Message | null) => {
+              console.log(" [x] Received %s", msg?.content.toString());
+            }, {noAck: true});
+
+            resolve();
+          }
+        });
+      }
+    });
+  });
+}
